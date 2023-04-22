05f94d8f9fee5cc8840a9da63b5e3f068157361f
Eule <felixweisselberg@web.de>
2023-04-22 23:42:37 +0200

    Projektdateien hinzufügen.

---
+++ NASAAPI_Image_Handler/LoginScreen.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace NASAAPI_Image_Handler
+{
+    /// <summary>
+    /// Interaktionslogik für LoginScreen.xaml
+    /// </summary>
+    public partial class LoginScreen : Window
+    {
+        public string uName = "f";
+        public string uMail = "mail@mail.de";
+        public string uPw = "123";
+        public LoginScreen()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            if (txtBoxUName.Text == uName && txtMail.Text == uMail && txtPW.Text == uPw)
+            {
+                MainWindow wndMain = new MainWindow();
+                wndMain.Show();
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show("Wrong Username, Password or E-Mail!");
+            }
+
+        }
+
+        private void btnQuit_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnCreateAcc_Click(object sender, RoutedEventArgs e)
+        {
+            CreateAcc wndCreateAcc = new CreateAcc();
+            wndCreateAcc.Show();
+        }
+    }
+}
