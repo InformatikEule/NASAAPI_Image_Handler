5086af99907fbe56bb3ebdb2ab21df12c7c550ad
Eule <felixweisselberg@web.de>
2023-12-10 19:09:23 +0100

    The user can now pick the Apod date! The DatePicker wasnt working previously.

--- NASAAPI_Image_Handler/pgApod.xaml.cs
+++ NASAAPI_Image_Handler/pgApod.xaml.cs
@@ -31,24 +31,30 @@ namespace NASAAPI_Image_Handler
         private void btnGo_Click(object sender,  RoutedEventArgs e)
         {
             GetAPOD();
+            //var pickedDateTest = dateApod?.SelectedDate?.ToString("yyyy-MM-dd").Split(' ')[0];
+            //var respTest = ($"https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date={pickedDateTest}asd");
+            //MessageBox.Show("Test hier: " + respTest);
         }
 
         private async void GetAPOD()
         {
-            var resp = await cl.GetAsync("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY");
+            var pickedDate = dateApod?.SelectedDate?.ToString("yyyy-MM-dd").Split(' ')[0];
+            var resp = await cl.GetAsync($"https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date={pickedDate}");
             if (resp.IsSuccessStatusCode)
             {
+                MessageBox.Show("resp hier :" + resp);
                 var content = await resp.Content.ReadAsStringAsync();
                 var apodData = JsonConvert.DeserializeObject<ApodData>(content);
                 txtApodTitle.Text = apodData?.title;
-                //DateTextBlock.Text = apodData.date;
+                //datetextblock.text = apoddata.date;
                 txtApodDesc.Text = apodData?.explanation;
                 BitmapImage img = new BitmapImage(new Uri(apodData.url));
                 PictureImage.Source = img;
             }
             else
             {
-                MessageBox.Show($"Failed to retrieve data. Status code: {resp.StatusCode}");
+                MessageBox.Show("resp hier :" + resp);
+                MessageBox.Show($"failed to retrieve data. status code: {resp.StatusCode}");
             }
         }
 
