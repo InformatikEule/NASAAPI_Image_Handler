9572d3a098adef7ec42cee608b35ac17892f39e4
InformatikEule <felixweisselberg@web.de>
2025-10-09 12:12:16 +0200

    working on Frontend for my smart freezer

--- NASAAPI_Image_Handler/pgFavs.xaml.cs
+++ NASAAPI_Image_Handler/pgFavs.xaml.cs
@@ -120,43 +120,43 @@ namespace NASAAPI_Image_Handler
         ///
         public static void FillUserListClass()
         {
-            var cmd = new MySqlCommand(query, conn);
-            cmd.Parameters.AddWithValue("@id", userId);
-            ///
-            Dictionary<string, User> UserList = new Dictionary<string, User>();
-            OleDbConnection conn = new OleDbConnection(strAccessConn);
-            string query = "SELECT ControlNumber, UserName FROM Log WHERE Log.EndStatus in ('Needs Review', 'Check Search', 'Vision Delivery', 'CA Review', '1TSI To Be Delivered');";
+            //var cmd = new MySqlCommand(query, conn);
+            //cmd.Parameters.AddWithValue("@id", userId);
+            /////
+            //Dictionary<string, User> UserList = new Dictionary<string, User>();
+            //OleDbConnection conn = new OleDbConnection(strAccessConn);
+            //string query = "SELECT ControlNumber, UserName FROM Log WHERE Log.EndStatus in ('Needs Review', 'Check Search', 'Vision Delivery', 'CA Review', '1TSI To Be Delivered');";
 
-            OleDbDataAdapter adapter = new OleDbDataAdapter(query, conn);
-            DataTable dt = new DataTable();
+            //OleDbDataAdapter adapter = new OleDbDataAdapter(query, conn);
+            //DataTable dt = new DataTable();
 
-            try
-            {
-                adapter.Fill(dt);
+            //try
+            //{
+            //    adapter.Fill(dt);
 
-                for (int ctr = 1; ctr <= dt.Rows.Count; ctr++)
-                {
-                    var row = dt.Rows[ctr];
-                    var userName = row["UserName"].ToString();
-                    if (!UserList.ContainsKey(userName))
-                    {
-                        UserList.Add(userName, new User()
-                        {
-                            UserName = userName,
-                            ControlNumber = new List<int>()
-                        });
-                    }
-                    UserList[userName].ControlNumber.Add((int)row["ControlNumber"]);
-                }
-            }
-            catch (OleDbException ex)
-            {
+            //    for (int ctr = 1; ctr <= dt.Rows.Count; ctr++)
+            //    {
+            //        var row = dt.Rows[ctr];
+            //        var userName = row["UserName"].ToString();
+            //        if (!UserList.ContainsKey(userName))
+            //        {
+            //            UserList.Add(userName, new User()
+            //            {
+            //                UserName = userName,
+            //                ControlNumber = new List<int>()
+            //            });
+            //        }
+            //        UserList[userName].ControlNumber.Add((int)row["ControlNumber"]);
+            //    }
+            //}
+            //catch (OleDbException ex)
+            //{
 
-            }
-            finally
-            {
-                conn.Close();
-            }
+            //}
+            //finally
+            //{
+            //    conn.Close();
+            //}
         }
     }
 }
